2a291ea947436c9ce31626c7d0991ab2404e6ba7
DaniHDZM <sayu142184@gmail.com>
2025-10-01 19:10:07 -0600

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import MiBoton from './components/Miboton'
+import ContadorClase from './components/ContadorClase'
+
+function App() {
+  
+
+  return (
+    <>
+      <ContadorClase/>
+      
+    </>
+  )
+}
+
+export default App
---
+++ src/components/ContadorClase.jsx
@@ -0,0 +1,35 @@
+import React, {Component} from "react";
+
+class ContadorClase extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            conteo: 0,
+        }
+    }
+
+    incrementar = () => {
+        this.setState((prevState) => ({ conteo: prevState.conteo + 1}));
+    }
+
+    reducir = () => {
+        this.setState((prevState) => ({ conteo: prevState.conteo - 1}));
+    }
+
+    render () {
+
+        const {conteo} = this.state;
+
+        return (
+            <div>
+                <h1>Contador</h1>
+                <p>Conteo: {conteo} </p>
+                <button onClick={this.incrementar}>Incrementar</button>
+                <button onClick={this.reducir}>Reducir</button>
+            </div>
+        );
+    }
+
+}
+export default ContadorClase;
